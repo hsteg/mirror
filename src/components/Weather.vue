31fcdc7f408c527a7969328070459f2af237d79e
Harrison Steg <hstegaa@gmail.com>
2020-07-14 23:35:45 -0400

    basic weather css grid

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -3,8 +3,18 @@
     <div v-if="isLoading">
       <Loading/>
     </div>
-    <div v-if="!isLoading" class="weather-info">
-      hello, current temp is: {{ weather.current.temp }}
+    <div v-if="!isLoading" class="weather-container">
+      <div class="current-weather">
+        current weather is {{ weather.current.feels_like }}
+      </div>
+      <div class="future-weather">
+        <div class="hourly">
+          hourly weather
+        </div>
+        <div class="daily">
+          daily weather
+        </div>  
+      </div>
     </div>
   </div>
 </template>
@@ -39,4 +49,35 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+  #weather {
+    width: 100%;
+
+    .weather-container {
+      width: 100%;
+      height: 500px;
+      border-radius: 15px;
+      border: solid 1px #e3e3e3;
+      display: grid;
+      grid-template-columns: 1fr 2fr;
+
+      .current-weather {
+        box-shadow: 1px 0 0 #404040;
+      }
+
+      .future-weather {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+
+        .hourly {
+          height: 50%;
+          box-shadow: 0 1px 0 #404040;
+        }
+
+        .daily {
+          height: 50%
+        }
+      }
+    }
+  }
 </style> 
