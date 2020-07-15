8f90e66824eda8607e7d350c1c77019d98058661
Harrison Steg <hstegaa@gmail.com>
2020-07-14 22:44:02 -0400

    Add basic loading spinner

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -1,25 +1,37 @@
 <template>
-  <div class="weather-container">
-    hello, current temp is: {{ weather.current.temp }}
+  <div id="weather">
+    <div v-if="isLoading">
+      <Loading/>
+    </div>
+    <div v-if="!isLoading" class="weather-info">
+      hello, current temp is: {{ weather.current.temp }}
+    </div>
   </div>
 </template>
 
 <script>
+import Loading from './Loading';
 
 export default {
   name: 'Weather',
   props: {},
+  components: {
+    Loading
+  },
   data() {
     return {
-      weather: {}
+      weather: {},
+      isLoading: false
     };
   },
   methods: {},
   created: function () {
+    this.isLoading = true;
       'method': 'GET'
     }).then(response => {
       return response.json();
     }).then(data => {
+      this.isLoading = false;
       this.weather = data;
     })
   }
