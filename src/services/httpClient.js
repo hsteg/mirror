2aa422df92a618cb35c4b5d7421d7d23a0ce3365
Harrison Steg <hstegaa@gmail.com>
2020-07-29 20:30:05 -0400

    update backend endpoints

--- src/services/httpClient.js
+++ src/services/httpClient.js
@@ -2,23 +2,32 @@ import axios from 'axios';
 
 export default {
   async getWeather() {
-    let res = await axios.get('http://192.168.1.180:8000/weather');
+    // http://192.168.1.180:8000
+    let res = await axios.get('https://protected-mesa-58440.herokuapp.com/weatherNow');
+    return res.data;
+  },
+  async getWeatherHourly() {
+    let res = await axios.get('https://protected-mesa-58440.herokuapp.com/weatherHourly');
+    return res.data;
+  },
+  async getWeatherDaily() {
+    let res = await axios.get('https://protected-mesa-58440.herokuapp.com/weatherDaily');
     return res.data;
   },
   async getTransit() {
-    let res = await axios.get('http://192.168.1.180:8000/transit');
+    let res = await axios.get('https://protected-mesa-58440.herokuapp.com/transit');
     return res.data;
   },
   async getB62south() {
-    let res = await axios.get('http://192.168.1.180:8000/b62south');
+    let res = await axios.get('https://protected-mesa-58440.herokuapp.com/b62south');
     return res.data;
   },
   async getB62north() {
-    let res = await axios.get('http://192.168.1.180:8000/b62north');
+    let res = await axios.get('https://protected-mesa-58440.herokuapp.com/b62north');
     return res.data;
   },
   async getSubwayStatus() {
-    let res = await axios.get('http://192.168.1.180:8000/mtaStatus');
+    let res = await axios.get('https://protected-mesa-58440.herokuapp.com/mtaStatus');
     return res.data;
   }
 }
